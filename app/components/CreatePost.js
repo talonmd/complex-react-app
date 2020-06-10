630b72f555413c5e8c7fa93383bf5ef6fe16dbcf
Talon Durrant <talonmdurrant@gmail.com>
2020-06-10 01:32:11 -0600

    finished lesson 39

--- app/components/CreatePost.js
+++ app/components/CreatePost.js
@@ -3,16 +3,18 @@ import Page from "./Page"
 import Axios from "axios"
 import { withRouter } from "react-router-dom"
 import DispatchContext from "../DispatchContext"
+import StateContext from "../StateContext"
 
 function CreatePost(props) {
   const [title, setTitle] = useState()
   const [body, setBody] = useState()
   const appDispatch = useContext(DispatchContext)
+  const appState = useContext(StateContext)
 
   async function handleSubmit(e) {
     e.preventDefault()
     try {
-      const response = await Axios.post("/create-post", { title, body, token: localStorage.getItem("complexappToken") })
+      const response = await Axios.post("/create-post", { title, body, token: appState.user.token })
       // redirect to new post url
       appDispatch({ type: "flashMessage", value: "Congrats. You have created a new post." })
       props.history.push(`/post/${response.data}`)
