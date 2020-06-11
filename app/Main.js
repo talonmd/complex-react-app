f5314fb646ce98189c635bfdc77c1ef023bcebc2
Talon Durrant <talonmdurrant@gmail.com>
2020-06-11 01:55:33 -0600

    finished lesson 63

--- app/Main.js
+++ app/Main.js
@@ -23,6 +23,7 @@ import Profile from "./components/Profile"
 import EditPost from "./components/EditPost"
 import NotFound from "./components/NotFound"
 import Search from "./components/Search"
+import Chat from "./components/Chat"
 
 function Main() {
   const initialState = {
@@ -34,6 +35,8 @@ function Main() {
       avatar: localStorage.getItem("complexappAvatar"),
     },
     isSearchOpen: false,
+    isChatOpen: false,
+    unreadChatCount: 0,
   }
 
   function ourReducer(draft, action) {
@@ -53,6 +56,18 @@ function Main() {
         break
       case "closeSearch":
         draft.isSearchOpen = false
+        break
+      case "toggleChat":
+        draft.isChatOpen = !draft.isChatOpen
+        break
+      case "closeChat":
+        draft.isChatOpen = false
+        break
+      case "incrementUnreadChatCount":
+        draft.unreadChatCount++
+        break
+      case "clearUnreadChatCount":
+        draft.unreadChatCount = 0
         break
     }
   }
@@ -106,6 +121,7 @@ function Main() {
           <CSSTransition timeout={330} in={state.isSearchOpen} classNames="search-overlay" unmountOnExit>
             <Search />
           </CSSTransition>
+          <Chat />
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
